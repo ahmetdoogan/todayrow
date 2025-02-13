e5db0e78b712074bb7b184bde8f03a76fe074aeb
Ahmet Doğan <ahmetdogan@MacBook-Air-2.local>
2025-02-13 22:46:28 +0300

    cron check methods changed to GET

--- src/pages/api/cron/check-subscription.ts
+++ src/pages/api/cron/check-subscription.ts
@@ -7,7 +7,7 @@ const supabase = createClient(
 );
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
+  if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
